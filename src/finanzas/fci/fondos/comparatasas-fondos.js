e614ea2079ec00a6378e9f5ca719138b80c8ca2b
enzonotario <enzo.notario@gmail.com>
2026-09-15 12:39:57 -0300

    fix: restore vinci-compass-liquidez-clase-f reference

--- src/finanzas/fci/fondos/comparatasas-fondos.js
+++ src/finanzas/fci/fondos/comparatasas-fondos.js
@@ -53,6 +53,6 @@ export const comparatasasFondos = [
   'ualintec-pesos-plus-clase-a',
   'ualintec-renta-dolares-clase-a',
   'ualintec-renta-variable-pesos-clase-a',
-  'vinci-compass-liquidez-clase-e',
   'vinci-compass-liquidez-clase-d',
+  'vinci-compass-liquidez-clase-f',
 ]
